cb2bc551eefab1ed77f43911455aa883b801fe59
zainkhartabel1995 <zainkhartabel1995@gmail.com>
2026-04-14 11:21:28 +0300

    #2: Design Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-
-const Card = ({title}) => {
-  const [hasLiked, setHasLiked] = useState(false);
-
-  return(
-    <div className='card'>
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '🤍'}
-      </button>
-    </div>
-  );
-
-}
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState(''); 
+
+
   return (
-    <>
-      <Card title="Iron Man"/>
-      <Card title="Avatar"/>
-    </>
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner"/>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
+      </div>
+    </main>
   );
 }
 
